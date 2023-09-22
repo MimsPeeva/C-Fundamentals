67681f606d527943a319c6b510c9c499c021df52
MimsPeeva <mpeeva04@abv.bg>
2023-09-22 16:10:26 +0300

    Add

---
+++ Data Types and Variables/Exact Sum of Real Numbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exact_Sum_of_Real_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double n = double.Parse(Console.ReadLine());
+            decimal sum = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                decimal number = decimal.Parse(Console.ReadLine());
+                sum += number;
+
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
